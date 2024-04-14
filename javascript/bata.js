22dcde5c0d51e2fdb403d5006068541d6efd80dd
sauravshete2003 <sauravshete72@gmail.com>
2024-04-14 18:24:16 +0530

    add change

--- javascript/bata.js
+++ javascript/bata.js
@@ -1,37 +1,34 @@
 
+
 function increment() {
     const quantityElement = document.getElementById('quantitynumber');
-    let quantitytext = parseInt(quantitynumber.innerText);
+    let quantitytext = parseInt(quantityElement.innerText);
     const pricetext = document.getElementById('price-text');
-
-    const pricetotal = document.getElementById('price-total')
+    const pricetotal = document.getElementById('price-total');
 
     if (quantitytext < 10) {
         quantityElement.innerText = ++quantitytext;
-
-    }
-    else {
-        alert("You can not increase quantity above 10.")
+    } else {
+        alert("You cannot increase quantity above 10.");
     }
 
-    pricetext.innerText = ` Price :  ${quantitytext} X  ₹ 499`;
+    pricetext.innerText = `Price :  ${quantitytext} X ₹ 499`;
     pricetotal.innerText = `Total ₹ : ${quantitytext * 499}`;
 }
-
 
 function decrement() {
     const quantityElement = document.getElementById("quantitynumber");
-    let quantitytext = parseInt(quantitynumber.innerText)
+    let quantitytext = parseInt(quantityElement.innerText);
     const pricetext = document.getElementById('price-text');
-    const pricetotal = document.getElementById('price-total')
+    const pricetotal = document.getElementById('price-total');
 
     if (quantitytext > 1) {
         quantityElement.innerText = --quantitytext;
+    } else {
+        alert("You cannot decrease quantity less than 1.");
     }
-    else {
-        alert("You can't descrese quantity less than 1")
-    }
-    pricetext.innerText = ` Price : ${quantitytext} X ₹ 499`;
-    pricetotal.innerText = `Total ₹ : ${quantitytext * 499}`;
 
+    pricetext.innerText = `Price : ${quantitytext} X ₹ 499`;
+    pricetotal.innerText = `Total ₹ : ${quantitytext * 499}`;
 }
+
