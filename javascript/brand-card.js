2e6a026a1d66d0a5b0d5904d6d7d0b2a32dbb307
Kvrathod348 <kvrathod348@gmail.com>
2024-04-15 00:32:25 +0530

    complete cards of nike brands

--- javascript/brand-card.js
+++ javascript/brand-card.js
@@ -7,7 +7,7 @@ function front(){
 function second(){
     const imageElement = document.getElementById('shoes');
     
-    imageElement.src = "../image/women-shoes/nike/air-max-plus-shoes-2.png"
+    imageElement.src = "../image/women-shoes/nike/air-max-plus-shoes-two.png"
 }
 
 function third(){
@@ -21,3 +21,78 @@ function forth(){
     
     imageElement.src = "../image/women-shoes/nike/air-max-plus-shoes-4.png"
 }
+// card 2
+
+function shoes1(){
+    const imageElement = document.getElementById('cardShoes2')
+    
+    imageElement.src = "../image/women-shoes/nike/b-air-max-sc-1.png"
+}
+
+function shoes2(){
+    const imageElement = document.getElementById('cardShoes2');
+    
+    imageElement.src = "../image/women-shoes/nike/b-air-max-sc-2.png"
+}
+
+function shoes3(){
+    const imageElement = document.getElementById('cardShoes2');
+    
+    imageElement.src = "../image/women-shoes/nike/b-air-max-sc-3.png"
+}
+
+function shoes4(){
+    const imageElement = document.getElementById('cardShoes3');
+    
+    imageElement.src = "../image/women-shoes/nike/b-air-max-sc-4.png"
+}
+// card 3
+function air1(){
+    const imageElement = document.getElementById('cardShoes3')
+    
+    imageElement.src = "../image/women-shoes/nike/c-air-jordan-2.jpeg"
+}
+
+function air2(){
+    const imageElement = document.getElementById('cardShoes3');
+    
+    imageElement.src = "../image/women-shoes/nike/c-air-jordan-3.jpeg" 
+}
+
+function air3(){
+    const imageElement = document.getElementById('cardShoes3');
+    
+    imageElement.src = "../image/women-shoes/nike/c-air-jordan-4.jpeg"
+}
+
+function air4(){
+    const imageElement = document.getElementById('cardShoes3');
+    
+    imageElement.src = "../image/women-shoes/nike/c-air-jordan-default.jpeg"
+}
+
+// card 4
+function gamma1(){
+    const imageElement = document.getElementById('cardShoes4')
+    
+    imageElement.src = "../image/women-shoes/nike/d-gamma-force-1.png"
+}
+
+function gamma2(){
+    const imageElement = document.getElementById('cardShoes4');
+    
+    imageElement.src = "../image/women-shoes/nike/d-gamma-force-2.png" 
+}
+
+function gamma3(){
+    const imageElement = document.getElementById('cardShoes4');
+    
+    imageElement.src = "../image/women-shoes/nike/d-gamma-force-3.png"
+}
+
+function gamma4(){
+    const imageElement = document.getElementById('cardShoes4');
+    
+    imageElement.src = "../image/women-shoes/nike/d-gamma-force-4.png"
+}
+
